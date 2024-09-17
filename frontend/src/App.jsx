6ab0d50d128a8d749f96d7be1aaa988ebdd0beb4
KYs <amitkys59@gmail.com>
2024-09-17 05:56:41 +0530

    git push testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Try } from "./assets/Components/Try";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">HI from fornted</h1>
+      <h1 className="text-3xl font-bold underline text-blue-500">HI from fornted</h1>
       <Try></Try>
     </>
   )
