747b5f9dd81ce254ba8a27b38f7224d60eaa2d12
KYs <amitkys59@gmail.com>
2024-09-17 06:04:15 +0530

    daisyUI installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Try } from "./assets/Components/Try";
+import { Navbar } from "./assets/Components/Navbar";
 function App() {
   return (
     <>
+    <Navbar></Navbar>
       <h1 className="text-3xl font-bold underline text-blue-500">HI from fornted</h1>
       <Try></Try>
     </>
