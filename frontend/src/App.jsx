90ea5ebb6bc7e5749f58f800857a911cd4e10ce1
KYs <amitkys59@gmail.com>
2024-09-17 05:40:26 +0530

    component test passed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-
+import React from "react";
+import { Try } from "./assets/Components/Try";
 function App() {
   return (
     <>
       <h1>HI from fornted</h1>
+      <Try></Try>
     </>
   )
 }
---
+++ frontend/src/assets/Components/Try.jsx
@@ -0,0 +1,8 @@
+// import React from "react"
+export function Try() {
+    return (
+        <>
+        <h1>Hi from component</h1>
+        </>
+    )
+}
