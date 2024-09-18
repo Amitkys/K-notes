49f8fe030d4c9ac14d9c656695f5607c276836de
KYs <amitkys59@gmail.com>
2024-09-18 06:14:27 +0530

    express basic biolerplate code

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Middleware to parse application/json
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-app.listen(3000, ()=>{
-    console.log('app is listening on 3000');
+app.post('/kys', (req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+    console.log(name, phone);
+    res.send('Data received');
 })
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+});
