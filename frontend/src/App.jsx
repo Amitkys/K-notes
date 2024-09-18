01bf6a86d76d2badde3f48a4827f317562d6d9d8
KYs <amitkys59@gmail.com>
2024-09-18 11:21:57 +0530

    data is fetch on fronted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,65 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [note, setNote] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3000/note')
     .then((res) => {
-      console.log(res.data.all_note);
+      // console.log(res.data.all_note);
+      setNote(res.data.all_note);
     })
   }, [])
 
 
   return (
-    <h1>This is react app</h1>
+    <div>
+      <h1>This is react app</h1>
+      <AllNotes notes={note}></AllNotes>
+    </div>
+    
+  )
+}
+
+
+
+function AllNotes({ notes }) {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+  };
+  return (
+    <div>
+      <div className='w-full grid grid-cols-3' style={{ gridTemplateColumns: '1fr 4fr 1fr' }}>
+        <div className='bg-red-500'>Left Column</div>
+        <div className='bg-green-500'>Middle Column</div>
+        <div className='bg-blue-500'>Right Column</div>
+      </div>
+      <div className='mt-4'>
+        {notes.map(note => (
+          <div key={note._id} className='grid grid-cols-3' style={{ gridTemplateColumns: '1fr 4fr 1fr' }}>
+            <div className='bg-red-500 p-2'>{formatDate(note.date)}</div>
+            <div className='bg-green-500 p-2'>{note.title}</div>
+            <div className='bg-blue-500 p-2 text-center'>
+              <button onClick={() => useEffect(()=> {
+                axios.get()
+              }, [])} className='btn btn-xs'>more</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+
+function ViewNote(){
+  return(
+    <div>
+
+    </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Allows access from your local network
+    port: 5173,       // Optional, ensures it's running on port 5173
+  },
   plugins: [react()],
   base: './'
 })
